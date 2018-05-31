0ba510894ba14ee00092f491699f502809c0fdc3
Clarissa Soares <clah@ufrn.edu.br>
2018-05-31 09:55:20 -0300

    Inserir e Excluir elemento da linked list da hashtable

--- yacc/hashtable.c
+++ yacc/hashtable.c
@@ -5,6 +5,11 @@
 
 #define HASH_SIZE 500
 
+const int ERRO1_SYMBOLINFO_JA_FOI_INSERIDO = -1;
+const int ERRO2_NAO_EXISTE_SYMBOLINFO = -2;
+
+const int SUCESSO = 1;
+
 
 typedef struct atributes_id {
   char * scope;
@@ -14,16 +19,15 @@ typedef struct atributes_id {
 } SymbolInfo;
 
 
-typedef struct ListElement {
+typedef struct node {
   SymbolInfo atributes;
-  struct ListElement *next;
+  struct node *next;
 } HashList;
 
 
 typedef struct hash_bucket {
-  HashList *front, *back;
+  HashList *head;
   int n_elems;
-  /* other stuff */
 } HashBucket;
 
 HashBucket hashTable[HASH_SIZE]; 
@@ -44,91 +48,120 @@ int hashCode(char* str) {
 
 void initialize(){
 	for (int i=0; i < HASH_SIZE; i++) {
-	  hashtable[i].front = NULL;
-	  hashtable[i].back = NULL;
+	  hashtable[i].head = NULL;
 	  hashtable[i].n_elems = 0;
+
 	}
+
 }
 
 
-struct SymbolInfo *search(char* key) {
+struct SymbolInfo *search(char* key, char*name, char*scope) {
    //get the hash 
    int hashIndex = hashCode(key);  
 	
    //pega na hashtable a linked list
+   if(hashTable[hashIndex] != NULL){
+   		HashList * current = hashTable[hashIndex]->head;
 
-   if(hashTable[hashIndex]->key){
+	    while (current != NULL) {
+	    	SymbolInfo* info = current->SymbolInfo;
 
-   }
+	    	if(strcmp(info.name, name) ==0 && strcmp(info.scope, scope) ==0){
+	    		return info;
+	    	}
 
-   //move in array until an empty 
-   while(hashTable[hashIndex] != NULL) {
-	
-      if(hashTable[hashIndex]->key == key)
-         return hashTable[hashIndex]; 
-			
-      //go to next cell
-      ++hashIndex;
-		
-      //wrap around the table
-      hashIndex %= SIZE;
-   }        
+	        //proximo
+	        current = current->next;
+	    }
+   }       
 	
    return NULL;        
 }
 
-void insert(int key,int data) {
-
-   struct DataItem *item = (struct DataItem*) malloc(sizeof(struct DataItem));
-   item->data = data;  
-   item->key = key;
-
-   //get the hash 
+int insert(char* key, char*name, char*scope, char* type) {
+	//get the hash 
    int hashIndex = hashCode(key);
 
-   //move in array until an empty or deleted cell
-   while(hashTable[hashIndex] != NULL && hashTable[hashIndex]->key != -1) {
-      //go to next cell
-      ++hashIndex;
-		
-      //wrap around the table
-      hashIndex %= SIZE;
+   SymbolInfo atribute = SymbolInfo malloc(sizeof(SymbolInfo));
+   atribute->name = name;
+   atribute->scope = scope;
+   atribute->type = type;
+
+   if(hashTable[hashIndex] != NULL){
+   		HashList * current = hashTable[hashIndex]->head;
+
+	    while (current != NULL) {
+	    	SymbolInfo* info = current->atributes;
+
+	    	//VERIFICA NA LISTA ENCADEADA SE O NOME JA NAO FOI DECLARADO NO ESCOPO.
+	    	if(strcmp(info.name, name) == 0 && strcmp(info.scope, scope) ==0){
+	    		return ERRO1_SYMBOLINFO_JA_FOI_INSERIDO;
+	    	}
+		    //proximo
+	        current = current->next;
+
+	    }
+
+	      /* now we can add a new variable */
+		    current->next = malloc(sizeof(HashList));
+		    current->next->head->atributes = atribute; 
+		   	current->next->head->next = NULL; 
+		   	hashTable[hashIndex]->n_elems++;
+		   	return SUCESSO;
+	    
+   }else{
+   	
+   	hashTable[hashIndex]->head = malloc(sizeof(HashList));
+   	hashTable[hashIndex]->head->atributes = atribute; 
+   	hashTable[hashIndex]->head->next = NULL; 
+   	hashTable[hashIndex]->n_elems++;
+   	return SUCESSO;
+   }   
+
+
+}
+
+int delete(char* key, char*name, char*scope) {
+	//get the hash 
+   int hashIndex = hashCode(key);
+   SymbolInfo retAtributes = NULL;
+
+   if(hashTable[hashIndex] != NULL){
+
+   		HashList * currentNode = hashTable[hashIndex]->head;
+   		HashList * tempNode = NULL;
+
+
+	    while (currentNode != NULL) {
+	    	SymbolInfo* info = currentNode->atributes;
+
+	    	//VERIFICA NA LISTA ENCADEADA SE TEM O SYMBOLINFO ESPECIFICADO PARA EXCLUIR.
+	    	if(strcmp(info.name, name) == 0 && strcmp(info.scope, scope) ==0){
+	    		tempNode = currentNode->next;
+			    retAtributes = tempNode->atributes;
+			    currentNode->next = tempNode->next;
+			    free(tempNode);
+			    hashTable[hashIndex]->n_elems--;
+	    		return SUCESSO;
+	    	}
+		    //proximo
+	        currentNode = currentNode->next;
+
+	    }
+
+	
    }
-	
-   hashTable[hashIndex] = item;
+   	
+   	return ERRO2_NAO_EXISTE_SYMBOLINFO;
+
 }
 
-struct DataItem* delete(struct DataItem* item) {
-   int key = item->key;
-
-   //get the hash 
-   int hashIndex = hashCode(key);
-
-   //move in array until an empty
-   while(hashTable[hashIndex] != NULL) {
-	
-      if(hashTable[hashIndex]->key == key) {
-         struct DataItem* temp = hashTable[hashIndex]; 
-			
-         //assign a dummy item at deleted position
-         hashTable[hashIndex] = dummyItem; 
-         return temp;
-      }
-		
-      //go to next cell
-      ++hashIndex;
-		
-      //wrap around the table
-      hashIndex %= SIZE;
-   }      
-	
-   return NULL;        
-}
 
 void display() {
    int i = 0;
 	
-   for(i = 0; i<SIZE; i++) {
+   for(i = 0; i<HASH_SIZE; i++) {
 	
       if(hashTable[i] != NULL)
          printf(" (%d,%d)",hashTable[i]->key,hashTable[i]->data);
